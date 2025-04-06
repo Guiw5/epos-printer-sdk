b7f0b685ef2c9e66597839dc0e385a2065bb689b
Guido Wagner <wagner.guido.5@gmail.com>
2025-04-06 14:35:47 -0300

    lib exports

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import { ePOSBuilder } from "./builders/ePOSBuilder";
+import { ePOSPrint } from "./builders/ePOSPrint";
+import { ePosDeviceMessage } from "./components/ePosDeviceMessage";
+import { ePOSDevice } from "./components/ePOSDevice";
+import { ePosCrypto } from "./components/ePosCrypto";
+import { CanvasPrint } from "./components/CanvasPrint";
+import { Printer } from "./devices/Printer";
+import { DeviceTerminal } from "./devices/DeviceTerminal";
+
+
+
+export { 
+  ePOSBuilder, 
+  ePOSPrint,
+  ePOSDevice, 
+  CanvasPrint,
+  ePosDeviceMessage, 
+  ePosCrypto,
+  Printer, 
+  DeviceTerminal
+};
